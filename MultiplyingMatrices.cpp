5c9c8ed588fe0da1f0d7eb7855ee42d0082a1b18
Ovidiu-Petrisor-Nicolae <ovidiu.nick@yahoo.com>
2017-03-16 22:12:43 +0200

    Update MultiplyingMatrices.cpp

--- MultiplyingMatrices.cpp
+++ MultiplyingMatrices.cpp
@@ -2,16 +2,14 @@
 #include<time.h>
 #include <stdlib.h>
 #define SIZE 10
-
+int i,j,k,firstMat[SIZE][SIZE], secondMat[SIZE][SIZE], rezMat[SIZE][SIZE];
 using namespace std;
 
+// Multiplying of 2 matrices ( with random numbers)
 
-int main(){
-  int i,j,k,firstMat[SIZE][SIZE], secondMat[SIZE][SIZE], rezMat[SIZE][SIZE];
+void createMat(){
   
   srand(time(NULL));
-  
-  
   for( i = 0; i < SIZE; i++)
     for( j = 0; j < SIZE; j++) {
       firstMat[i][j] = rand() & 100;
@@ -21,9 +19,11 @@ int main(){
     for( j = 0; j < SIZE; j++) {
       secondMat[i][j] = rand() & 100;
     }
-   
-   
-  for( i = 0; i < SIZE; i++){
+}
+
+void showMat(){
+  
+ for( i = 0; i < SIZE; i++){
     for( j = 0; j < SIZE; j++) {
       cout<<first[i][j]<<" ";     
     }
@@ -32,7 +32,15 @@ int main(){
   
   cout<<"\n\n";
   
-  
+   for( i = 0; i < SIZE; i++){
+    for( j = 0; j < SIZE; j++) {
+      cout<<second[i][j]<<" ";     
+    }
+    cout<<"\n";
+  }
+}
+
+void multiplyMat(){
   for( i = 0; i < SIZE; i++)
     for( j = 0; j < SIZE; j++) {
       rezMat[i][j] = 0;
@@ -51,5 +59,12 @@ int main(){
     }
    cout<<"\n"; 
   }
-    return 0;
+}
+          
+int main(){
+  
+  createRandomMat();
+  showRandomMat();
+  multiplyMat();
+  return 0;
 }
