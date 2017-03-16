cfe7918b03781e33760f87b2530f87eb3efc14ed
Ovidiu-Petrisor-Nicolae <ovidiu.nick@yahoo.com>
2017-03-16 22:26:57 +0200

    Update MultiplyingMatrices.cpp

--- MultiplyingMatrices.cpp
+++ MultiplyingMatrices.cpp
@@ -1,69 +1,71 @@
 #include <iostream>
 #include<time.h>
 #include <stdlib.h>
-#define SIZE 10
+#define SIZE 5
 int i,j,k,firstMat[SIZE][SIZE], secondMat[SIZE][SIZE], rezMat[SIZE][SIZE];
 using namespace std;
 
-// Multiplying of 2 matrices ( with random numbers)
+// Multiplying of 2 matrices
 
 void createMat(){
-  
+
   srand(time(NULL));
   for( i = 0; i < SIZE; i++)
     for( j = 0; j < SIZE; j++) {
-      firstMat[i][j] = rand() & 100;
+      firstMat[i][j] = rand() & 10;
     }
-  
+
   for( i = 0; i < SIZE; i++)
     for( j = 0; j < SIZE; j++) {
-      secondMat[i][j] = rand() & 100;
+      secondMat[i][j] = rand() & 10;
     }
 }
 
 void showMat(){
-  
+
  for( i = 0; i < SIZE; i++){
     for( j = 0; j < SIZE; j++) {
-      cout<<first[i][j]<<" ";     
+      cout<<firstMat[i][j]<<" ";
     }
     cout<<"\n";
   }
-  
+
   cout<<"\n\n";
-  
+
    for( i = 0; i < SIZE; i++){
     for( j = 0; j < SIZE; j++) {
-      cout<<second[i][j]<<" ";     
+      cout<<secondMat[i][j]<<" ";
     }
     cout<<"\n";
   }
 }
 
 void multiplyMat(){
-  for( i = 0; i < SIZE; i++)
-    for( j = 0; j < SIZE; j++) {
-      rezMat[i][j] = 0;
-    }
-   
-  
-  for( i = 0; i < SIZE; i++)
-    for( j = 0; j < SIZE; j++) 
-      for( rezMat[i][j] += firstMat[i][k] * secondMat[k][j]; 
-    }
-  
-  
-  for( i = 0; i < SIZE; i++){
-    for( j = 0; j < SIZE; j++) {
-      cout<<rez[i][j]<<" ";
-    }
-   cout<<"\n"; 
-  }
+
+    for( i = 0; i < SIZE; i++)
+        for( j = 0; j < SIZE; j++)
+            rezMat[i][j] = 0;
+
+    for (i = 0; i < SIZE; i++)
+		for (j = 0; j < SIZE; j++)
+			for (int k = 0; k < SIZE; k++)
+                rezMat[i][j] += firstMat[i][k] * secondMat[k][j];
+
+    cout<<"\n";
+
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++) {
+            cout<<rezMat[i][j]<<" ";
+        }
+        cout<<"\n";
+        }
 }
-          
+
 int main(){
-  createRandomMat();
-  showRandomMat();
-  multiplyMat();
-  return 0;
+
+    createMat();
+    showMat();
+    multiplyMat();
+
+    return 0;
 }
