a98178963bdf478e87c9a5eadd6a80da6e99a51e
Ovidiu-Petrisor-Nicolae <ovidiu.nick@yahoo.com>
2017-03-16 21:10:22 +0200

    Create MultiplyingMatrices.cpp

---
+++ MultiplyingMatrices.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<time.h>
+#include <stdlib.h>
+#define SIZE 10
+
+using namespace std;
+
+
+int main(){
+  int i,j,k,firstMat[SIZE][SIZE], secondMat[SIZE][SIZE], rezMat[SIZE][SIZE];
+  
+  srand(time(NULL));
+  
+  
+  for( i = 0; i < SIZE; i++)
+    for( j = 0; j < SIZE; j++) {
+      firstMat[i][j] = rand() & 100;
+    }
+  
+  for( i = 0; i < SIZE; i++)
+    for( j = 0; j < SIZE; j++) {
+      secondMat[i][j] = rand() & 100;
+    }
+   
+   
+  for( i = 0; i < SIZE; i++){
+    for( j = 0; j < SIZE; j++) {
+      cout<<first[i][j]<<" ";     
+    }
+    cout<<"\n";
+  }
+  
+  cout<<"\n\n";
+  
+  
+  for( i = 0; i < SIZE; i++)
+    for( j = 0; j < SIZE; j++) {
+      rezMat[i][j] = 0;
+    }
+   
+  
+  for( i = 0; i < SIZE; i++)
+    for( j = 0; j < SIZE; j++) 
+      for( rezMat[i][j] += firstMat[i][k] * secondMat[k][j]; 
+    }
+  
+  
+  for( i = 0; i < SIZE; i++){
+    for( j = 0; j < SIZE; j++) {
+      cout<<rez[i][j]<<" ";
+    }
+   cout<<"\n"; 
+  }
+    return 0;
+}
